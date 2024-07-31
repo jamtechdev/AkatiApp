976702a839cc0a33173eb3364b87f39719ab4961
Jamtechdev <jamtechba@gmail.com>
2024-07-31 17:36:51 +0530

    asd

--- src/components/CustomHeader.jsx
+++ src/components/CustomHeader.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Colors } from '../_utils/GlobalStyle';
+import {View, Text, StyleSheet} from 'react-native';
+import {Colors} from '../_utils/GlobalStyle';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-const CustomHeader = ({ title }) => {
+const CustomHeader = ({title}) => {
   return (
     <View style={styles.header}>
       <Text style={styles.title}>{title}</Text>
-      <Ionicons
-                name={'notifications'}
-                size={22}
-                color={'white'}
-              />
+      <Ionicons name={'notifications'} size={22} color={'white'} />
     </View>
   );
 };
@@ -19,7 +15,7 @@ const styles = StyleSheet.create({
   header: {
     height: 50,
     justifyContent: 'space-between',
-    flexDirection:'row',
+    flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: 20,
     backgroundColor: Colors.tertiary, // Adjust as needed
