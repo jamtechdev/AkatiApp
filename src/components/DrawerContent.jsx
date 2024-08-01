a63b6b4ca66aee713fdf7b3163bc6ce234d20191
preeti <webApktest@gmail.com>
2024-08-01 11:58:48 +0530

    drawer fixed

--- src/components/DrawerContent.jsx
+++ src/components/DrawerContent.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+// src/components/DrawerContent.js
+import React from 'react'
+import {View, Text, StyleSheet, Image} from 'react-native'
 import {
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
-} from '@react-navigation/drawer';
-import logo from '../images/logo.png';
-import {Colors} from '../_utils/GlobalStyle';
-const DrawerContent = (props, {navigation}) => {
+} from '@react-navigation/drawer'
+import logo from '../images/logo.png'
+import {Colors} from '../_utils/GlobalStyle'
+
+const DrawerContent = props => {
   return (
     <DrawerContentScrollView
       {...props}
@@ -19,32 +21,40 @@ const DrawerContent = (props, {navigation}) => {
         />
         <Text style={styles.drawerTitle}>My App</Text>
       </View>
-      <DrawerItemList {...props} />
+
       <DrawerItem
-        label="Help"
-        onPress={() => props.navigation.navigate('Settings')} // Navigate to Settings screen
+        label='Settings'
+        labelStyle={{color: Colors.white}}
+        onPress={() => props.navigation.navigate('Settings')}
       />
-      {/* Add more custom drawer items here */}
+          <DrawerItem
+        label='Review'
+        labelStyle={{color: Colors.white}}
+        onPress={() => props.navigation.navigate('Review')}
+      />
+          {/* <DrawerItem
+        label='Logout'
+        labelStyle={{color: Colors.white}}
+        onPress={() => props.navigation.navigate('Settings')}
+      /> */}
     </DrawerContentScrollView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   drawerHeader: {
-    // padding: 20,
     alignItems: 'center',
   },
   drawerTitle: {
     fontSize: 20,
-    fontWeight: 800,
+    fontWeight: 'bold',
     color: Colors.white,
   },
   profileImage: {
     width: 80,
     height: 80,
-    // borderRadius: 40,
     marginBottom: 10,
   },
-});
+})
 
-export default DrawerContent;
+export default DrawerContent
--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,35 +1,31 @@
+// src/navigation/TabNavigator.js
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {LibraryScreen, DiscoverScreen, SearchScreen} from '../screen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { LibraryScreen, DiscoverScreen, SearchScreen } from '../screen';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import {CustomHeader, CustomTabBar} from '../components';
+import { CustomHeader, CustomTabBar } from '../components';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator tabBar={props => <CustomTabBar {...props} />}>
+    <Tab.Navigator
+      tabBar={props => <CustomTabBar {...props} />}
+      screenOptions={({ navigation, route }) => ({
+        header: (props) => <CustomHeader {...props} title={route.name} navigation={navigation} />,
+      })}
+    >
       <Tab.Screen
         name="Home"
         component={DiscoverScreen}
-        options={{
-          header: () => <CustomHeader title="Home" />,
-        }}
       />
       <Tab.Screen
         name="Library"
-        and
         component={LibraryScreen}
-        options={{
-          header: () => <CustomHeader title="Library" />,
-        }}
       />
       <Tab.Screen
         name="Search"
         component={SearchScreen}
-        options={{
-          header: () => <CustomHeader title="Search" />,
-        }}
       />
     </Tab.Navigator>
   );
