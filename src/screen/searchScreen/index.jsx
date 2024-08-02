d42b95632ca545fd52876862bcf4c1fc40e504a5
Jamtechdev <jamtechba@gmail.com>
2024-08-02 18:03:41 +0530

    search

--- src/screen/searchScreen/index.jsx
+++ src/screen/searchScreen/index.jsx
@@ -1,60 +1,80 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import React from 'react';
 import {
   CustomText,
+  GradientView,
   HeadingText,
   RowContainer,
   SkeletonLoader,
   TextBadge,
 } from '../../components';
-import { TextInput } from 'react-native-gesture-handler';
-import { Colors } from '../../_utils/GlobalStyle';
+import {TextInput} from 'react-native-gesture-handler';
+import {Colors} from '../../_utils/GlobalStyle';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const categories = ['Happy', 'Sad', 'Exciting', 'Mystery', 'Fantasy', 'Sci-Fi', 'Romance', 'Thriller'];
+const categories = [
+  'Happy',
+  'Sad',
+  'Exciting',
+  'Mystery',
+  'Fantasy',
+  'Sci-Fi',
+  'Romance',
+  'Thriller',
+];
 
 export default function SearchScreen() {
   return (
     <RowContainer>
       <View>
+        <GradientView style={styles.searchIcon}>
+          <Icon size={25} color={Colors.white} name={'search'} />
+        </GradientView>
         <TextInput
           placeholder="Which book would you like to read today..."
           style={styles.searchBar}
           placeholderTextColor={Colors.darkGray}
         />
-        <View style={styles.searchIcon}>
-          <Icon size={25} color={Colors.white} name={'search'} />
-        </View>
       </View>
       <ScrollView>
-      <SkeletonLoader isLoading />
-      <View>
-        <HeadingText>Select Book Categories</HeadingText>
-        <View style={styles.categoryContainer}>
-          {categories.map((category, index) => (
-            <TextBadge key={index} text={category} />
-          ))}
+        <View>
+          <HeadingText>Select Book Categories</HeadingText>
+          <View style={styles.categoryContainer}>
+            {categories.map((category, index) => (
+              <TextBadge key={index} text={category} />
+            ))}
+          </View>
         </View>
-      </View>
-      <View>
-        <HeadingText>Hot search</HeadingText>
-        <View style={styles.categoryContainer}>
-          {categories.map((category, index) => (
-            <TextBadge key={index} text={category} />
-          ))}
+        <View>
+          <HeadingText>Hot search</HeadingText>
+          <View style={styles.categoryContainer}>
+            {categories.map((category, index) => (
+              <TextBadge key={index} text={category} />
+            ))}
+          </View>
         </View>
-      </View>
-      <View>
-        <HeadingText>Recent search</HeadingText>
-        <View style={{marginVertical:10}}>
-          {categories.map((category, index) => (
-            <View style={styles.RecentContainer} key={index}>
-                <Text style={{color:Colors.white,fontWeight:"bold",fontSize:12}}>{category}</Text>
-                <Icon size={25} color={Colors.white} name={'close-circle-outline'} />
-            </View>
-          ))}
+        <View>
+          <HeadingText>Recent search</HeadingText>
+          <View style={{marginVertical: 10}}>
+            {categories.map((category, index) => (
+              <View style={styles.RecentContainer} key={index}>
+                <Text
+                  style={{
+                    color: Colors.white,
+                    fontWeight: 'bold',
+                    fontSize: 12,
+                  }}>
+                  {category}
+                </Text>
+                <Icon
+                  size={25}
+                  color={Colors.white}
+                  name={'close-circle-outline'}
+                />
+              </View>
+            ))}
+          </View>
         </View>
-      </View>
       </ScrollView>
     </RowContainer>
   );
@@ -71,15 +91,15 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   searchIcon: {
-    width: 40,
-    height: 40,
-    backgroundColor: Colors.secondary,
+    width: 37,
+    height: 35,
+    // backgroundColor: Colors.secondary,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 50,
     position: 'absolute',
-    right: 5,
-    top: 4,
+    right: 8,
+    top: 5,
     bottom: 0,
     margin: 'auto',
   },
@@ -89,16 +109,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginVertical: 10,
   },
-  RecentContainer:{
-    width:"100%",
-    justifyContent:"space-between",
-    alignItems:"center",
-    flexDirection:"row",
-    padding:8,
-    marginVertical:5,
-    borderWidth:1,
-    borderColor:Colors.darkGray,
-    borderRadius:5
-
-  }
+  RecentContainer: {
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 8,
+    marginVertical: 5,
+    borderWidth: 1,
+    borderColor: Colors.darkGray,
+    borderRadius: 5,
+  },
 });
