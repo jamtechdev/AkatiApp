6f5713077c10ed1a37ab80fd9cb9f4e2d5bada48
Jamtechdev <jamtechba@gmail.com>
2024-10-10 12:43:08 +0530

    config

--- src/_constant/Config.js
+++ src/_constant/Config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://author.akatibird.com/api/customer';
-export const IMAGE_API_URL = 'https://author.akatibird.com/storage/book-front-cover/';
+export const API_URL = 'https://dev.akatibird.com/api/customer';
+export const IMAGE_API_URL = 'https://dev.akatibird.com/storage/book-front-cover/';
 export const PAYPAL_CLIENT_ID =
   'AdUP4boW0BB7uxglGu0hNwP8RFVsZ2W7b3nN0Dgj9kMrWtRI_FbRHVKKf7ciuiD-0hpMVEHYnj0R63Hf';
 export const CINETPAY_API_ID = '695644265ece900561f2c6.42532258';
